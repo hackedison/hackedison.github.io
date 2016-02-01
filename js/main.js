1fc058c88bae5ade8cd3139308408f1e78f5e926
Tarun Boddupalli <tarunbod@gmail.com>
2016-01-31 19:04:29 -0500

    tweaked navbar style, fixed navbar highlight

--- js/main.js
+++ js/main.js
@@ -11,24 +11,26 @@ $('.logo').on('click', function(e) {
 	}, 1000);
 });
 
-var scrollPoints = [
-	0,
-	525,
-	1085,
-	1610,
-	2170,
-	2519,
-	10000
-];
+var scrollPoints = [];
+$('.slide').each(function(i) {
+	var posObj = {
+		pos: $(this).position().top,
+		type: $(this).hasClass('bg-primary')
+	};
+	scrollPoints[i] = posObj;
+});
 
 $(window).on('scroll', function() {
-	var scrollIndex;
+	var scrollIndex = 5;
 	for (var i = 1; i < scrollPoints.length; i++) {
 		var scrollPos = document.body.scrollTop;
-		if (scrollPos >= (scrollPoints[i - 1] - 100) && scrollPos < scrollPoints[i]) {
+		var scrollPoint1 = scrollPoints[i - 1];
+		var scrollPoint2 = scrollPoints[i];
+		if (scrollPos >= (scrollPoint1.pos-150) && scrollPos < scrollPoint2.pos) {
 			scrollIndex = i - 1;
 		}
 	}
+	console.log(scrollIndex);
 	$('.navbar-custom-nav').removeClass('active');
 	$($('.navbar-custom-nav').get(scrollIndex)).addClass('active');
 });
@@ -44,5 +46,5 @@ $('.question-answer a').smoothScroll(defaultScrollOptions);
 $('.navbar-custom-nav.middle a').smoothScroll({
     speed: 'auto',
     autoCoefficient: 1,
-    offset: -250
+    offset: -100
 });
